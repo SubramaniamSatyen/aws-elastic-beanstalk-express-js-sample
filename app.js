7a5df60f4e21e18f563c20f3e7b547baa4d66869
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-05-10 12:28:35 -0700

    Updating Code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
+
 
 app.get('/', (req, res) => res.send('Hello Hack Cloud !'));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
 
-git 
