2d248ba9d99fab3d217b268a6259d0810de5442c
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-05-11 20:21:38 -0700

    Cool Commit

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.port || 8080;
 
 
-app.get('/', (req, res) => res.send('Goodbye Hack Cloud!'));
+app.get('/', (req, res) => res.send('Hello Hack Cloud !'));
 
 app.listen(port, () => {
     console.log(`App running on http://localhost:${port}`);
 });
+
+
